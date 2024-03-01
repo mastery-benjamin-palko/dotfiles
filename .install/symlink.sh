d3b5b3bdb28a6662c1ff6be188a87eb2df40347f
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-03-01 14:05:27 -0500

    add symlinks
    link .zshrc, .tool-versions and nvim, k9s config directories

---
+++ .install/symlink.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# files
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.tool-versions ~/.tool-versions
+
+# directories
+ls -s ~/dotfiles/nvim/ ~/.config/nvim/
+ls -s ~/dotfiles/k9s/ ~/.config/k9s/
+

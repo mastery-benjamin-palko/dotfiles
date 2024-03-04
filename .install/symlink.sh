189e92ef82479322df6e9c367cfb595aff219d1f
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-03-04 08:17:48 -0500

    check if directories exist first

--- .install/symlink.sh
+++ .install/symlink.sh
@@ -5,6 +5,10 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.tool-versions ~/.tool-versions
 
 # directories
-ls -s ~/dotfiles/nvim/ ~/.config/nvim/
-ls -s ~/dotfiles/k9s/ ~/.config/k9s/
+if test -d ~/.config/nvim/; then
+  ls -s ~/dotfiles/nvim/ ~/.config/nvim/
+fi
+if test -d ~/.config/k9s; then
+  ls -s ~/dotfiles/k9s/ ~/.config/k9s/
+fi
 

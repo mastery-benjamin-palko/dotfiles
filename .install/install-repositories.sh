d5666eb0db0edf003655e65bd32aeea06eee3eb3
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-03-04 08:31:28 -0500

    install curl for repositories shell

--- .install/install-repositories.sh
+++ .install/install-repositories.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt-get install curl
 # pgadmin4 repo
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list

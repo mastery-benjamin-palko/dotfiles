6e9dab456e9e72265a1075a9e9593c7b222817f6
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-03-04 12:01:43 -0500

    sudo and fix if

--- .install/install-repositories.sh
+++ .install/install-repositories.sh
@@ -3,10 +3,10 @@
 sudo apt-get install curl
 
 # pgadmin4 repo
-if ![ -f /usr/share/keyrings/packages-pgadmin-org.gpg ]; then
+if [ ! -f /usr/share/keyrings/packages-pgadmin-org.gpg ]; then
   curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 fi
-echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list
+sudo echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list
 
 sudo apt-get update
 

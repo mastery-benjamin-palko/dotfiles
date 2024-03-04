76dc072bfdce3f4b180f68b4e3940741103fe356
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-03-04 10:00:20 -0500

    wtf am I smoking...

--- .install/brew-install.sh
+++ .install/brew-install.sh
@@ -3,3 +3,9 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew --version
 
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/benjaminpalko/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+source ~/.bashrc
+brew --version
+
--- .install/zsh-setup.sh
+++ .install/zsh-setup.sh
@@ -6,9 +6,3 @@ chsh -s chsh -s $(which zsh)
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/benjaminpalko/.zshrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-source ~/.bashrc
-brew --version
-

3a4ca2da3d4f24ecc070c87255e1315b59487252
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-03-04 10:52:31 -0500

    install or update brew

--- .install/brew-install.sh
+++ .install/brew-install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew --version
+if [[ $(command -v brew) == "" ]]; then
+  echo "Installing brew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/benjaminpalko/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-source ~/.bashrc
+  (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/benjaminpalko/.bashrc
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  source ~/.bashrc
+else
+  echo "Updating brew..."
+  brew update
+fi
 brew --version
 

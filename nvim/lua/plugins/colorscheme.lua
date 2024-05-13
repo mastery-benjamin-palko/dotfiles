ff65ec0ca786d76807b158d376157abb335c6bab
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-05-13 13:42:06 -0400

    switch to LazyVIm

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent_mode = true,
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
+  },
+}
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,64 +1,42 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    config = function()
-      require "configs.conform"
-    end,
-  },
+  -- {
+  --   "pmizio/typescript-tools.nvim",
+  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  --   opts = {},
+  -- },
 
   {
-    "williamboman/mason.nvim",
+    "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        -- lua
-        "lua-language-server",
-        "stylua",
-
-        -- web dev
-        "html-lsp",
-        "css-lsp",
-        "css-variables-language-server",
-        "prettier",
-        "tailwindcss-language-server",
-        "eslint-lsp",
-        "graphql-language-service-cli",
-        "js-debug-adapter",
-
-        -- shell
-        "shfmt",
-        "bash-language-server",
-        "bash-debug-adapter",
-
-        -- docker
-        "docker-compose-language-server",
-        "dockerfile-language-server",
-
-        -- go
-        "gopls",
-        "go-debug-adapter",
-
-        -- python
-        "debugpy",
-
-        -- web debug
-        "firefox-debug-adapter",
-
-        -- markdown
-        "marksman",
-
-        -- sql
-        "sqls",
-
-        -- configs
-        "json-lsp",
-        "yaml-language-server"
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "cpp",
+        "go",
+        "sql"
       },
     },
   },
 
   {
-    "mfussenegger/nvim-dap",
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        sql = { "sqlfmt" }
+      }
+    },
   },
 
   {
@@ -69,102 +47,5 @@ return {
       automatic_installation = true,
       ensure_installed = { "js", "firefox", "bash", "python" },
     },
-  },
-
-  {
-    "williamboman/mason-lspconfig.nvim",
-    cmd = { "LspInstall", "LspUninstall" },
-    config = function()
-      require("mason-lspconfig").setup()
-    end,
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "vimdoc",
-        "html",
-        "css",
-        "javascript",
-        "jsdoc",
-        "typescript",
-        "tsx",
-        "markdown",
-        "markdown_inline",
-        "json",
-        "jsonc",
-        "yaml",
-        "graphql",
-        "go",
-        "bash",
-        "dockerfile",
-        "gitignore",
-      },
-    },
-  },
-
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    lazy = false,
-    opts = {},
-  },
-
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-  },
-
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/neotest-jest",
-    },
-    config = function()
-      require("neotest").setup {
-        adapters = {
-          require "neotest-jest" {
-            jestCommand = "yarn test --",
-            -- jestConfigFile = "custom.jest.config.ts",
-            -- env = { CI = true },
-            cwd = function(path)
-              return vim.fn.getcwd()
-            end,
-          },
-        },
-      }
-    end,
-  },
-
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup {
-        -- Configuration here, or leave empty to use defaults
-      }
-    end,
-  },
-
-  {
-    "b0o/schemastore.nvim",
-    version = "*",
   },
 }

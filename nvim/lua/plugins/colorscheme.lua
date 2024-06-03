1c565f37609079f68d1c72ed6f0d26597cf595c1
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-06-03 10:52:26 -0400

    update lazyvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -15,12 +15,23 @@ return {
       transparent_mode = true,
     },
   },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require("everforest").setup({
+        transparent_background_level  = 1
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
     opts = {
-      transparent_background = true,
+      colorscheme = "everforest",
     },
   },
 }
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,51 +1,46 @@
+local js_based_languages = { "typescript", "javascript", "typescriptreact" }
+
+for _, language in ipairs(js_based_languages) do
+  require("dap").configurations[language] = {
+    {
+      type = "node",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
+    },
+    {
+      type = "node",
+      request = "launch",
+      name = "Launch via Yarn",
+      runtimeExecutable = "yarn",
+      cwd = "${workspaceFolder}",
+      port = 5858,
+      preLaunchTask = "tsc: build - tsconfig.json",
+      outFiles = {
+        "${workspaceFolder}/dist/**/*.js",
+      },
+    },
+    {
+      type = "node",
+      request = "attach",
+      name = "Attach to Remote",
+      address = "localhost",
+      port = 9229,
+      localRoot = "${workspaceFolder}/dist",
+      remoteRoot = "/opt/carrier/dist",
+      preLaunchTask = "tsc: build - tsconfig.json",
+      outFiles = { "${workspaceFolder}/dist/**/*", "!**/node_modules/**" },
+      skipFiles = { "<node_internals>/**/*" },
+      sourceMaps = true,
+    },
+  }
+end
+
 return {
   -- {
   --   "pmizio/typescript-tools.nvim",
   --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
   --   opts = {},
   -- },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "cpp",
-        "go",
-        "sql"
-      },
-    },
-  },
-
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        sql = { "sqlfmt" }
-      }
-    },
-  },
-
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    dependencies = "mason.nvim",
-    cmd = { "DapInstall", "DapUninstall" },
-    opts = {
-      automatic_installation = true,
-      ensure_installed = { "js", "firefox", "bash", "python" },
-    },
-  },
 }

121c9c907154d874b7e4bd1a214e3a27d62371eb
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-07-04 17:10:55 -0400

    remove personal project

--- .install/setup-development.sh
+++ .install/setup-development.sh
@@ -15,5 +15,4 @@ git clone https://github.com/masterysystems/customer.git ${dev}/customer
 git clone https://github.com/masterysystems/employee.git ${dev}/employee
 git clone https://github.com/masterysystems/facility.git ${dev}/facility
 git clone https://github.com/masterysystems/mastery-frontend.git ${dev}/mastery-frontend
-git clone https://github.com/mastery-benjamin-palko/ornn.git ${dev}/ornn
 

1ad9db0ec036d36ebb773677775dbc4ad5f7f023
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-07-04 13:48:13 -0400

    update clusters

--- .install/kubernetes/clusters.sh
+++ .install/kubernetes/clusters.sh
@@ -7,6 +7,7 @@ az aks get-credentials --resource-group nonprod-cluster-eastus2 --name aks-cgl60
 az aks get-credentials --resource-group nonprod-cluster-eastus2 --name aks-cat7x-nonprod --subscription cat
 az aks get-credentials --resource-group nonprod-cluster-eastus2 --name aks-co2yt-nonprod --subscription covenant
 az aks get-credentials --resource-group nonprod-cluster-eastus2 --name aks-dg3nl-nonprod --subscription dollargeneral
+az aks get-credentials --resource-group nonprod-cluster-eastus2 --name aks-eva9o-nonprod --subscription evans
 az aks get-credentials --resource-group nonprod-cluster-eastus2 --name aks-hir3j-nonprod --subscription hirschbach
 az aks get-credentials --resource-group nonprod-cluster-eastus2 --name aks-kdp5t-nonprod --subscription kdp
 az aks get-credentials --resource-group nonprod-cluster-eastus2 --name aks-ls8fm-nonprod --subscription loadsmith
@@ -17,6 +18,7 @@ az aks get-credentials --resource-group nonprod-cluster-centralus --name aks-ru1
 az aks get-credentials --resource-group nonprod-cluster-centralus --name aks-sn100-nonprod --subscription schneider
 az aks get-credentials --resource-group nonprod-cluster-centralus --name aks-tm100-nonprod --subscription tmc
 az aks get-credentials --resource-group nonprod-cluster-centralus --name aks-tb100-nonprod --subscription trailerbridge
+az aks get-credentials --resource-group nonprod-cluster-eastus2 --name aks-usmmg-nonprod --subscription multimodalgroup
 az aks get-credentials --resource-group nonprod-cluster-centralus --name aks-we100-nonprod --subscription werner
 
 # production
@@ -24,15 +26,18 @@ az aks get-credentials --resource-group prod-cluster-centralus --name aks-ave4u-
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-cgl60-prod --subscription cargill
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-cat7x-prod --subscription cat
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-co2yt-prod --subscription covenant
+az aks get-credentials --resource-group prod-cluster-centralus --name aks-dg3nl-prod --subscription dollargeneral
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-eva9o-prod --subscription evans
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-hir3j-prod --subscription hirschbach
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-kdp5t-prod --subscription kdp
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-ls8fm-prod --subscription loadsmith
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-ml100-prod --subscription molo
+az aks get-credentials --resource-group prod-cluster-centralus --name aks-pep5c-prod --subscription pepsi
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-pr100-prod --subscription prime
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-ru100-prod --subscription ruan
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-sn100-prod --subscription schneider
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-tm100-prod --subscription tmc
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-tb100-prod --subscription trailerbridge
+az aks get-credentials --resource-group prod-cluster-centralus --name aks-usmmg-prod --subscription multimodalgroup
 az aks get-credentials --resource-group prod-cluster-centralus --name aks-we100-prod --subscription werner
 

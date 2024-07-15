4ccadb2638f6bd50768a3d584a1ed6857727f3e3
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-07-15 18:15:03 -0400

    update nix install script

--- .install/install-nix.sh
+++ .install/install-nix.sh
@@ -2,3 +2,12 @@
 
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
+source ~/.bashrc
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+
+nix-shell '<home-manager>' -A install
+
+home-manager switch
+

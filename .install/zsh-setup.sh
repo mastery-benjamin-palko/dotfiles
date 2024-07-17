8a415e9d7b4591b3845812c1cee7cd4bf21f5cc6
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-07-17 18:25:52 -0400

    change to only set shell

--- .install/zsh-setup.sh
+++ .install/zsh-setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 # Set shell to zsh
-chsh -s $(which zsh)
-
-# Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-source ~/.zshrc
+chsh -s "$(which zsh)"
 

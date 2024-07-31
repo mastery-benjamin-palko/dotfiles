b1e900a1bf1905dddc985b34389025bca6ec63b3
Benjamin Palko <ext.benjamin.palko@mastery.net>
2024-07-31 16:51:28 -0400

    use flake install

--- .install/install-home-manager.sh
+++ .install/install-home-manager.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --add https://nixos.org/channels/nixos-unstable nixpkgs
-nix-channel --update
-
-nix-shell '<home-manager>' -A install
-home-manager switch --flake "$HOME"/dotfiles
+nix run home-manager/master -- switch --flake "$HOME"/dotfiles
 
